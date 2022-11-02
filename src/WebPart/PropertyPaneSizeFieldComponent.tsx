769055acfe85e78dc313b66e93d60684d0480783
Nikolay Belykh <nbelyh@gmail.com>
2022-11-02 15:22:11 +0100

    Fix the defaults for full width and teams

--- src/WebPart/PropertyPaneSizeFieldComponent.tsx
+++ src/WebPart/PropertyPaneSizeFieldComponent.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import { Dropdown, IDropdownOption, Stack, TextField, Text } from '@fluentui/react';
 
+const makeSplitValue = (value: string) => {
+  const matches = value?.match(/(\d+)\s*(\w+|%)?/);
+  return {
+    number: matches?.[1] ?? '',
+    units: matches?.[2] ?? ''
+  };
+};
+
 export function PropertyPaneSizeFieldComponent(props: {
   value: string;
   setValue: (value: string) => void;
@@ -22,31 +30,44 @@ export function PropertyPaneSizeFieldComponent(props: {
     { key: 'px', text: "pixels" },
   ];
 
-  const [value, setValue] = React.useState(props.value);
+  const splitValue = makeSplitValue(props.value);
+  const [number, setNumber] = React.useState(splitValue.number);
+  const [units, setUnits] = React.useState(splitValue.units);
+
+  const debounce = React.useRef(0);
+
+  const [valueToSave, setValueToSave] = React.useState(props.value);
   React.useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (value) {
-        props.setValue(value);
-      } else {
-        props.getDefaultValue().then(defaultVal => {
-          setValue(defaultVal);
-        });
-      }
-    }, 1000);
+    if (valueToSave) {
+      props.setValue(valueToSave);
+    }
+  }, [valueToSave]);
+
+  const processChanges = () => {
+    if (number && units) {
+      setValueToSave(number + units);
+    } else {
+      props.getDefaultValue().then(defaultValue => {
+        const splitDefaultValue = makeSplitValue(defaultValue);
+        setNumber(splitDefaultValue.number);
+        setUnits(splitDefaultValue.units);
+        setValueToSave(defaultValue);
+      });
+    }
+    debounce.current = 1000;
+  };
+
+  React.useEffect(() => {
+    const timeout = setTimeout(processChanges, debounce.current);
     return () => clearTimeout(timeout);
-  }, [value]);
-
-  const matches = value.match(/(\d+)\s*(\w+|%)?/);
-
-  const number = matches?.[1] ?? '';
-  const units = matches?.[2] ?? screen;
+  }, [number, units]);
 
   const onNumberChanged = async (_, val) => {
-    setValue(val + units);
+    setNumber(val);
   };
 
   const onUnitChanged = (_, val) => {
-    setValue(number + val.key);
+    setUnits(val.key);
   };
 
   return (
